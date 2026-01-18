2fc8356611ab50ea3fa849acd5af52c020c30834
rifqihendrawan <rifqihendrawan81@gmail.com>
2026-01-18 15:40:33 +0700

    Update prediksaham.py

--- prediksaham.py
+++ prediksaham.py
@@ -7,6 +7,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from tensorflow.keras.models import Sequential
